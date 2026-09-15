10b92a2b7ebd19f2873795e265b06249d700f9d8
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-09-15 21:44:00 +0600

    postman collection added

--- src/server.ts
+++ src/server.ts
@@ -6,19 +6,21 @@ import { prisma } from "./lib/prisma";
 const port = config.port;
 
 
-async function main() {
-  try {
-    console.log("server starting ...")
-    await prisma.$connect();
-    console.log("Database connected successfully");
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  } catch (error) {
-    console.log("Error starting the server", error);
-    prisma.$disconnect();
-    process.exit(1);
-  }
-}
+// async function main() {
+//   try {
+//     console.log("server starting ...")
+//     await prisma.$connect();
+//     console.log("Database connected successfully");
+//     app.listen(port, () => {
+//       console.log(`Server running on port ${port}`);
+//     });
+//   } catch (error) {
+//     console.log("Error starting the server", error);
+//     prisma.$disconnect();
+//     process.exit(1);
+//   }
+// }
 
-main();
+// main();
+
+export default app;
